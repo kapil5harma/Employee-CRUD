b6f110f7234315bda9394aaf2397e61075c1e8be
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-12-20 02:21:44 +0530

    Basic App header included

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <div className='navbar'>
+          <h2 className='center '>Employee CRUD</h2>
+        </div>
         <EmployeeForm />
         <EmployeeList />
       </div>
